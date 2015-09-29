7a88bdae56048fcb79b24f06a24c93a7ded05dbc
Mia Nordentoft <mia@miestasmia.com>
2015-09-29 22:04:09 +0200

    Added about menu to tray icon

--- gui/perdyshot.py
+++ gui/perdyshot.py
@@ -1,15 +1,46 @@
 #!/usr/bin/env python2
 
-import os, sys, subprocess
-from PyQt4 import QtGui
+import os, sys, subprocess, signal
+from PyQt4 import QtGui, QtCore
 from gi.repository import Notify
 
 dir = os.path.dirname(os.path.realpath(__file__))
 cwd = os.getcwd()
 
 ICON = dir + "/../icon_glow.png"
+LOGO = dir + "/../icon_plain.png"
+VERSION = 'Perdyshot ' + open(dir + '/../.version', 'r').read()
+URL = "https://github.com/Miestasmia/Perdyshot"
 
 Notify.init("Perdyshot")
+
+app = QtGui.QApplication(sys.argv)
+
+# Create about dialog
+class AboutDialog(QtGui.QWidget):
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+        self.setFixedSize(450, 230)
+        self.setWindowTitle("About Perdyshot")
+
+        image = QtGui.QLabel(self)
+        image.setPixmap(QtGui.QPixmap(LOGO))
+        image.move((450 - image.sizeHint().width()) / 2, 10)
+
+        text = QtGui.QLabel(self)
+        text.setText("<b>%s &copy; 2015 Mia Nordentoft. MIT License</b>" % VERSION)
+        text.move((450 - text.sizeHint().width()) / 2, image.sizeHint().height() + 30)
+
+        website = QtGui.QPushButton("Website", self)
+        website.move((450 - website.sizeHint().width()) / 2, image.sizeHint().height() + 60)
+        website.clicked.connect(self.openWebsite)
+
+    def closeEvent(self, event):
+        event.ignore()
+        self.hide()
+
+    def openWebsite(self):
+        subprocess.call(["xdg-open", URL])
 
 
 # Create tray icon
@@ -24,6 +55,7 @@ class SystemTrayIcon(QtGui.QSystemTrayIcon):
 
         menu.addSeparator()
 
+        menu.addAction("About", self.onAbout)
         menu.addAction("Quit", self.onQuit)
 
         self.setContextMenu(menu)
@@ -31,15 +63,20 @@ class SystemTrayIcon(QtGui.QSystemTrayIcon):
     def onQuit(self):
         sys.exit(0)
 
+    def onAbout(self):
+        aboutDialog.show()
+
     def onActiveWindowCapture(self):
         subprocess.call(["/usr/bin/env", "python2", dir + "/../cli/window.py", "--delay", "0"])
         notification = Notify.Notification.new("Screenshot saved", "", ICON)
         notification.show()
 
 
+aboutDialog = AboutDialog()
 
-app = QtGui.QApplication(sys.argv)
-w = QtGui.QWidget()
-trayIcon = SystemTrayIcon(QtGui.QIcon(ICON), w)
+iconWidget = QtGui.QWidget()
+trayIcon = SystemTrayIcon(QtGui.QIcon(ICON), iconWidget)
 trayIcon.show()
-sys.exit(app.exec_())
+
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+app.exec_()
