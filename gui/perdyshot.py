8d777f869cb5b685cc595b7296af5414ef2ba1c5
yrsegal <yrsegal@gmail.com>
2015-10-17 23:59:56 -0400

    couldn't get gi to work
    this will allow use without needing gi
    just no notifications

--- gui/perdyshot.py
+++ gui/perdyshot.py
@@ -8,7 +8,10 @@ import os, sys, subprocess, signal, tempfile, shutil, pipes, time, locale
 from datetime import datetime
 
 from PyQt4 import QtGui, QtCore
-from gi.repository import Notify
+try:
+    from gi.repository import Notify
+except:
+    Notify = None
 
 dirname = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(dirname, os.path.pardir))
@@ -27,7 +30,8 @@ URL = "https://github.com/Miestasmia/Perdyshot"
 DATE = os.path.getmtime(os.path.join(dirname, os.path.pardir, ".version"))
 DATE = datetime.fromtimestamp(DATE).strftime(locale.nl_langinfo(locale.D_T_FMT))
 
-Notify.init("Perdyshot")
+if Notify:
+    Notify.init("Perdyshot")
 
 
 config = ConfigObj(os.path.join(dirname, os.path.pardir, 'perdyshot.conf'), encoding = 'UTF8', configspec = os.path.join(dirname, os.path.pardir, 'perdyshot.conf.spec'))
@@ -132,7 +136,7 @@ class SystemTrayIcon(QtGui.QSystemTrayIcon):
             if options['copy']:
                 subprocess.call('xclip -i -sel clip < ' + pipes.quote(filename), shell = True)
 
-            if options['notification']:
+            if options['notification'] and Notify:
                 notification = Notify.Notification.new(
                     options['notificationTitle'],
                     options['notificationDescription'],
